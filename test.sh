cbb101cc6d64ed5c6973e119fc53178f8f43172a
alexgre <alexgre@ufl.edu>
2017-11-07 13:48:24 -0500

    test10

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-ssh -tt -i "alexgre1.pem" ubuntu@ec2-34-201-172-56.compute-1.amazonaws.com
+ssh -i "alexgre1.pem" ubuntu@ec2-34-201-172-56.compute-1.amazonaws.com /bin/bash <<'EOT'
+sudo docker stop $(docker ps -q --filter ancestor=alexgre/angulardocker1.0)
+sudo docker pull alexgre/app_latest
+sudo docker run --rm -p 80:80 alexgre/app_latest
+EOT
