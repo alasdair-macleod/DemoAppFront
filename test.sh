f2616dcb3e5662d9080ddd14943f9cbf787e0747
alexgre <alexgre@ufl.edu>
2017-11-08 11:24:13 -0500

    test14

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i "alexgre1.pem" ubuntu@ec2-34-201-172-56.compute-1.amazonaws.com /bin/bash << 'EOT'
 Xy123456 | sudo docker login -u alexgre --password-stdin
 sudo docker stop $(docker ps -q --filter ancestor=alexgre/angulardocker1.0)
-sudo docker pull alexgre/app_latest
-sudo docker run -d --rm -p 80:80 alexgre/app_latest
+sudo docker pull alexgre/aapp
+sudo docker run -d --rm -p 80:80 alexgre/aapp
 EOT
