9588620bf63858d12832c1ba1c10411dd0f6dbde
alexgre <alexgre@ufl.edu>
2017-11-21 09:54:02 -0500

    tar problem3

--- docker_perserve.sh
+++ docker_perserve.sh
@@ -8,5 +8,7 @@ VERSION=$(node -e "console.log(require('./package.json').version)")
 #build image
 docker build -t $REPO/$IMAGE:latest -t $REPO/$IMAGE:$VERSION .
 #upload the image to AWS ECR repo
+./ecr_credentials.sh
+$(aws ecr get-login --no-include-email --region us-east-1)
 docker push $REPO/$IMAGE:latest
 docker push $REPO/$IMAGE:$VERSION
