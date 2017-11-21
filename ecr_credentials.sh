dafad2eae49d19954531b52c6ec62ef6a2cc156a
alexgre <alexgre@ufl.edu>
2017-11-21 12:31:55 -0500

    aws login5

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -1,6 +1,7 @@
-mkdir -p ~/.aws
 cat > ~/.aws/credentials /bin/bash << EOL
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
+
+eval $(aws ecr get-login --no-include-email --region us-east-1)
